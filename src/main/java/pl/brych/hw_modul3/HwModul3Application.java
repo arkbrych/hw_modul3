110c41b33226c185e39bf1e54f0f377d1e4527ba
abrych <arkadiusz.brych@external.t-mobile.pl>
2020-04-20 16:52:53 +0200

    resolved conflict between hateoas and swagger

--- src/main/java/pl/brych/hw_modul3/HwModul3Application.java
+++ src/main/java/pl/brych/hw_modul3/HwModul3Application.java
@@ -1,8 +1,15 @@
 package pl.brych.hw_modul3;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class HwModul3Application {
@@ -11,4 +18,10 @@ public class HwModul3Application {
         SpringApplication.run(HwModul3Application.class, args);
     }
 
+    @Bean
+    public LinkDiscoverers discovers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
 }
