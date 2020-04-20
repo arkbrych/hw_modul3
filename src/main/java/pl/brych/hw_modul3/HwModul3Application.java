9851f0b0d6b460b904df33a5a35fe876294e1bf7
abrych <arkadiusz.brych@external.t-mobile.pl>
2020-04-20 16:16:50 +0200

    hateoas initial commit

--- src/main/java/pl/brych/hw_modul3/HwModul3Application.java
+++ src/main/java/pl/brych/hw_modul3/HwModul3Application.java
@@ -1,6 +1,7 @@
 package pl.brych.hw_modul3;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
